ef59320189fc4a414256fb194ce992cafcbf1285
L165 <91383297+Lapetite165@users.noreply.github.com>
2025-11-14 12:49:56 +0100

    Destruction v8165131
    En changeant la taille des éléments, la destruction ne fonctionne plus.

--- Assets/Scripts/BreakingPot.cs
+++ Assets/Scripts/BreakingPot.cs
@@ -3,35 +3,34 @@ using UnityEngine;
 
 public class BreakingPot : MonoBehaviour
 {
-    public GameObject Pot, BrokenPot;
+	public GameObject Pot, BrokenPot;
 
-    void OnClick()
-    {
-        Pot.SetActive(false);
-        BrokenPot.SetActive(true);
-    }
+	void OnClick()
+	{
+		Pot.SetActive(false);
+		BrokenPot.SetActive(true);
+	}
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Pot.SetActive(true);
-    }
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		Pot.SetActive(true);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        for (int i = 0; i < Input.touchCount; ++i) {
-                if (Input.GetTouch(i).phase == TouchPhase.Began) {
-                // Construct a ray from the current touch coordinates
-                Touch touch = Input.GetTouch(i);
-                //float radius = touch.radius + touch.radiusVariance;
-                Ray ray = Camera.main.ScreenPointToRay(touch.position);
-                    // Create a particle if hit
-                    if (Physics.Raycast(ray, 3f)) {
-                        OnClick();
-                        Debug.Log("Clicked !");
-                    }
-                }
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		for (int i = 0; i < Input.touchCount; ++i) {
+			Touch touch = Input.GetTouch(i);
+			if (Input.GetTouch(i).phase == TouchPhase.Began) {
+				// Construct a ray from the current touch coordinates
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
+				// Create a particle if hit
+				if (Physics.Raycast(ray, 3f)) {
+					OnClick();
+					Debug.Log("Clicked !");
+				}
+			}
+		}
+	}
 }
